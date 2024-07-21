2e137003a92ef012d674f49f2705835a2d78cfc4
El-Shamel <143452112+El-Shamel@users.noreply.github.com>
2024-07-21 14:08:48 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
     return {"message": "Hello, You See Api Mohamed Helal!",
             "startapi":"/docs"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+
+if __name__ =="__main__":
+    uvicorn.run("main:app",host="0.0.0.0",port="8080",reload=True)
